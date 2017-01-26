704eae2954f42c3e40cab4cd855d463bbff6392d
liuyi123 <296114106@qq.com>
2017-01-27 07:56:04 +0800

    Initial Commit

---
+++ 斗鱼TV/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  斗鱼TV
+//
+//  Created by liuyi on 17/1/27.
+//  Copyright © 2017年 liuyi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
